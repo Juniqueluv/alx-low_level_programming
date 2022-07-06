b0db84d9b272ba601ea2e367c8fe17c8da6f2822
olajumoke <oolajumoke099@gmail.com>
2022-07-06 05:07:07 -0700

    debugging task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+
+void positive_or_negative(int=i){
+	int n;
+
+srand(time(0));
+n = i;
+if (n > 0)
+	printf("%i is positive\n", n);
+if (n==0)
+	printf("%i is zero\n", n);
+if (n < 0)
+{
+	printf("%i is negative\n", n);
+}
+return (0);
+}
+/**
+ * main - debugging example
+ * Return: 0
+ *
+ int main(void)
+ {
+ 	int i;
+
+	i=0;
+	positive_or_negative(i);
+	void positive_or_negative(int i)
+	return (0);
+}
