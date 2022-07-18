023a135414087d270fcd4bf2557692d4837a798c
olajumoke <oolajumoke099@gmail.com>
2022-07-18 15:03:46 -0700

    push code

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,8 +13,8 @@ void print_chessboard(char (*a)[8])
 	for (index1 = 0; a[index1][7]; index1++)
 	{
 		for (index2 = 0; index2 < 8; index2++)
-			putchar(a[index1][index2]);
+			_putchar(a[index1][index2]);
 		
-		putchar('\n');
+		_putchar('\n');
 	}
 }
