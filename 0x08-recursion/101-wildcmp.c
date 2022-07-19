dae1906cc3fc36c23b24287bb54d06f2b2792d44
olajumoke <oolajumoke099@gmail.com>
2022-07-19 14:45:34 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* wildcmp - Compares two strings.
+* wild-cmp - Compares two strings.
 * @s1: 1st string to be compared
 * @s2: second string containing '*' or not.
 * Return: If the strings are considered to be indentical - 1
 * If the strings are not identical - 0.
 */
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2);
 {
 	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
