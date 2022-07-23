082b771e3f67560314ffba6385985dbf4b7d3513
olajumoke <oolajumoke099@gmail.com>
2022-07-22 21:53:56 -0700

    push agrs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - Prints its name, followed by a new line.
-* @argc: Number of command line arguments.
-* @argv: array that contains the program command line arguments.
-* Return: 0 - success.
+* main - function
+* @argc: offset count
+* @argv: offset value
+* Return: 0
 */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
-
+	printf("%s\n", *argv);
 	return (0);
 }
