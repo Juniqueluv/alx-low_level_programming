ed0c4ff503795887c2caee5ef910f252e7b226ce
olajumoke <oolajumoke099@gmail.com>
2022-08-03 17:16:07 -0700

    corrected task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @f: A pointer to a function that prints a name
  * Return: No return
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 		f(name);
