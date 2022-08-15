69bf34e7c7841a6e929a10469e93cfaa22077795
juniqueluv <oolajumoke099@gmail.com>
2022-08-15 06:36:28 -0700

    correct code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,31 +1,28 @@
-#include <stdlib.h>
-
-#include <string.h>
+#include "lists.h"
 
 #include <stdio.h>
 
-#include "lists.h"
-
-
-
 /**
-*main - check the code
-* Return: Always 0.
-*/
+
+ * print_list - Prints all the elements of a list_t list
+
+ * @h: The list_t list
+
+ * Return: The number of nodes in h.
+
+ */
+
 
 int main(void)
-
-/**
-* print_list - Prints all the elements of a list_t list
-* @h: The list_t list
-* Return: The number of nodes in h.
-*/
-
 size_t print_list(const list_t *h);
+
 {
+
 	size_t nodes = 0;
 
-	while(h); 
+
+
+	while (h)
 
 	{
 
