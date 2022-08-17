8d23a232ef32a4c50f85b552d854c26073ae6407
olajumoke <106930636+Juniqueluv@users.noreply.github.com>
2022-08-17 02:08:22 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
-* insert_nodeint_at_index - Inserts a new node to a listint_t
-*                           list at a given position.
-* @head: A pointer to the address of the
-*        head of the listint_t list.
-* @idx: The index of the listint_t list where the new
-*       node should be added - indices start at 0.
-* @n: The integer for the new node to contain.
-*
-* Return: If the function fails - NULL.
-*         Otherwise - the address of the new node.
-*/
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *copy = *head;
@@ -22,6 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
+
 	if (idx == 0)
 	{
 		new->next = copy;
@@ -37,8 +38,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		copy = copy->next;
 	}
 
-	new->next = copy->next
-		copy->next = new;
+	new->next = copy->next;
+	copy->next = new;
 
 	return (new);
 }
